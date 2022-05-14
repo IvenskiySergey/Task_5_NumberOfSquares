21c1a7bd2a2ab30e1d24b3d56f34291474205b95
Сергей Ивеснкий <ivenskijsv@gmail.com>
2022-05-15 01:49:48 +0300

    Add tests

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,7 +4,7 @@ public class main {
 
     public static void main(String[] args) {
         SQRService service = new SQRService();
-        int numbersOfSQR = service.calculate(200,300);
+        int numbersOfSQR = service.calculate(200, 300);
         System.out.println("Колличество квадратов: " + numbersOfSQR);
     }
 }
--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,14 +1,14 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int calculate (int min, int max) {
+    public int calculate(int min, int max) {
         int count = 0;
         for (int i = 10; i < 100; i++) {
-            if (i * i >=min) {
-                if (i * i <=max) {
+            if (i * i >= min) {
+                if (i * i <= max) {
                     count++;
                 }
-                }
+            }
         }
         return count;
     }
